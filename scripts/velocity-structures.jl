58a814bb4944d08b8d3c3f0399fa5ca7a28a0ec2
fjebaker <fergusbkr@gmail.com>
2024-10-18 14:22:13 +0100

    chore: refactor velocity structures

--- scripts/velocity-structures.jl
+++ scripts/velocity-structures.jl
@@ -1,3 +1,8 @@
+# -------- ONLY DEFINE FUNCTIONS AND STRUCTS IN THIS FILE ------------- #
+# Use a seperate runner script (see e.g. plot-velocity.jl) to create
+# an executable script. This file will be `include`d in other files, and
+# any top-level code will be executed in all of them.
+
 using Gradus, Plots
 
 function logrange(first, last, num)
@@ -24,17 +29,6 @@ function soundspeed(r)
     T^2
 end
 
-m = KerrMetric(1.0, 0.998)
-
-# log range from the innermost stable circular orbit (inner edge of the
-# accretion disc) out to some arbitrary radius
-radii = logrange(Gradus.isco(m), 1000, 200)
-
-# if you need it, these will be the azimuthal angles to each "radial" bin
-θ = collect(range(0, 2π, 100))
-
-keplerian_velocities = Gradus.CircularOrbits.fourvelocity.(m, radii)
-
 # -------- HERE IS WHERE YOU NEED TO NOW MODIFY THE VELOCITIES -------- #
 
 # below is just a trivial example
@@ -51,52 +45,3 @@ function turbulent_structure(m, r, correlation_length)
     Gradus.constrain_all(m, x, v, 1.0)
 end
 
-# then apply however your velocity function works to the keplerian_velocities
-turbulent_velocities = turbulent_structure.(m, radii, 0.0)
-
-# --------------------------------------------------------------------- #
-
-# some visualisation methods (use the ones that make the most sense to you)
-# note these are all only using the 4th component of the velocity vector (which
-# is the azimuthal component):
-# 1 - time
-# 2 - radial (r)
-# 3 - poloidal (θ)
-# 4 - azimuthal (ϕ)
-begin
-    plot(
-        radii,
-        [i[4] for i in keplerian_velocities],
-        xlabel = "r",
-        ylabel = "vᵩ",
-        xscale = :log10,
-        yscale = :log10,
-        label = "keplerian",
-    )
-    plot!(radii, [i[4] for i in turbulent_velocities], label = "turbulent")
-end
-
-begin
-    keplerian_field =
-        [log10(i[4]) for (i, r) in zip(keplerian_velocities, radii), angle in θ]
-    heatmap(θ, radii, keplerian_field, projection = :polar, title = "keplerian")
-end
-
-begin
-    turbulent_field = [log10(turbulent_structure(m, r, 0.0)[4]) for r in radii, angle in θ]
-    heatmap(θ, radii, turbulent_field, projection = :polar, title = "turbulent")
-end
-
-begin
-    comparison_map = [
-        (turbulent_structure(m, r, 0.0)[4] - v[4]) / v[4] for
-        (v, r) in zip(keplerian_velocities, radii), angle in θ
-    ]
-    heatmap(
-        θ,
-        radii,
-        comparison_map,
-        projection = :polar,
-        title = "(turbulent - keplerian) / keplerian",
-    )
-end
