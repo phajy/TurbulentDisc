0586417b0ce8fa3e995905ee0168479b1c76e0a5
katebolam <katebolam.x@icloud.com>
2024-11-11 11:28:57 +0000

    Upload Pariev & Bromley turbulent velocity profile scripts

---
+++ klb_scripts/pariev-bromley-equations-klb.jl
@@ -0,0 +1,71 @@
+# Following equations are taken from Pariev & Bromley 1998 prescription of velocity. 
+
+# Eddington Luminosity function
+function eddington_luminosity(M)
+    Ledd_constant = 1.2e46  # Base Eddington luminosity in ergs/s for 10^8 solar masses
+    return Ledd_constant * (M / 1e8)  # M should be in units of solar masses (M☉)
+end
+
+# Equation 4
+function A(r, a)
+    return 1 + ((a^2) / r^2) + (2 * (a^2) / r^3) 
+end
+
+# Equation 5
+function B(r, a)
+    return 1 + (a / r^(3/2))
+end
+
+# Equation 6
+function C(r, a)
+    return 1 - (3 / r) + ((2 * a) / r^(3/2))
+end
+
+# Equation 7
+function D(r, a)
+    return 1 - (2 / r) + ((a^2) / r^2)
+end
+
+# Equation 8
+function E(r, a)
+    return 1 + (4 * (a^2) / r^2) - (4 * (a^2) / r^3) + (3 * (a^4) / r^4)
+end
+
+# Equation 9
+function F(r, a)
+    return 1 - (2 * a / r^(3/2)) + (a^2 / r^2)
+end
+
+# Equation 10
+function G(r, a)
+    return 1 - (2 / r) + (a / r^(3/2))
+end
+
+# Equation 11
+function J(r, a)
+    numerator = 1 + (a / r^(3/2))
+    denominator = (1 - (3 / r) + (2 * a / r^(3/2)))^(1/2)
+    return numerator / denominator
+end
+
+# Equation 12
+function L_func(r, a, r_ms) # ('_func' to not mistake with L, the luminosity)
+    term1 = F(r, a) / sqrt(C(r, a))
+    inner_term = 1 - (2 * a) / (3 * sqrt(r_ms))
+    if inner_term < 0
+        error("The argument inside the root becomes negative.")
+    end
+    term2 = (2 * sqrt(3) / sqrt(r)) * inner_term
+    return term1 - term2
+end
+
+# Equation 15: Speed of sound to speed of light ratio (v_turb)
+function sound_speed_ratio(r, a, epsilon, L, L_edd, r_ms)
+    factor = 1.18 * epsilon^(-1) * (L / L_edd) * r^(-3/2)
+    A_val = A(r, a)
+    B_val = B(r, a)
+    D_val = D(r, a)
+    E_val = E(r, a)
+    L_val = L_func(r, a, r_ms)
+    return factor * A_val * B_val^(-2) * D_val^(-1/2) * E_val^(-1/2) * L_val
+end
---
+++ klb_scripts/plot-velocities-vs-r-klb.jl
@@ -0,0 +1,37 @@
+# Script plots the turbulent velocity using Pariev & Bromley 1998 prescription, as a function of radius in the disc.
+
+# Import relevant libraries and modules
+using Plots
+
+# Include the Pariev & Bromley functions script
+include("pariev-bromley-equations-klb.jl")
+
+# Define constants
+epsilon = 0.1  # Disc efficiency parameter (example value)
+luminosity = 1e46  # Disc luminosity in erg/s (example value)
+M_black_hole = 1e8 # Typical mass of black hole (10^8 solar masses)
+edd_luminosity_val = eddington_luminosity(M_black_hole) # Eddington luminosity for a 10^8 solar mass black hole
+r_ms = 1.237*M_black_hole # ISCO radius
+
+# Define a range of radii (r) for the plot
+radii = collect(range(1.0, stop=25.0, length=200))  # example radial range
+
+# Define values for the spin parameter a/M
+spin_parameters = [0.0, 0.5, 0.9, 0.99, 0.998]
+
+# Create an empty plot object
+plt = plot(
+    xlabel = "Radius (r/M)",
+    ylabel = "Turbulent Velocity (cₛ / c)",
+    title = "Turbulent Velocity vs. Disc Radius",
+    legend = :topright,
+)
+
+# Calculate and plot turbulent velocity for each spin parameter
+for a in spin_parameters
+    turbulent_velocities = [sound_speed_ratio(r, a, epsilon, luminosity, edd_luminosity_val, r_ms) for r in radii]
+    plot!(plt, radii, turbulent_velocities, label="a/M = $a")
+end
+
+# Show plot
+display(plt)
