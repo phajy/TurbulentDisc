aa712d78768f6aaa2300ab0b500af5367309390e
jj-auer <joschka.auer@gmail.com>
2025-02-05 12:31:56 +0000

    Added a new file to visualise the Sound Speed function, so as to keep things modular

---
+++ jj-auer_scripts/VisualiseSoundSpeed.jl
@@ -0,0 +1,22 @@
+using Plots
+include("SoundSpeed.jl")
+
+M = 1.0
+alpha = 0.1
+lum = 1e46
+
+plt = plot(
+    xlabel = "Radius (r/M)",
+    ylabel = "Sound Speed (v/c)",
+    title = "Sound Speed vs. Radius",
+    legend = :topright,
+)
+
+rPos = collect(range(1.0, stop=25.0, length=500))
+
+for a in [0, 0.5, 0.90, 0.99, 0.998]
+    m = KerrMetric(M = M, a = a)
+    plot!(rPos, [SoundSpeed(m, r, a, M, lum) for r in rPos], label="a/M = $a")
+end
+
+display(plt)
