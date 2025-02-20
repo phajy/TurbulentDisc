7f4d5dd8d782aaec61e14e5efe14f9867c5f3e83
katebolam <katebolam.x@icloud.com>
2025-02-20 13:35:20 +0000

    refactor: update ray traced geodesic image script to match updated turbulence functions (normalised using Pariev and Bromley sound speed equation)

--- klb_scripts/ray-tracer-klb.jl
+++ klb_scripts/ray-tracer-klb.jl
@@ -1,10 +1,16 @@
+# import required libraries
+using Plots, Gradus, LaTeXStrings
+
 # import the velocity functions
 include("velocity-structures-klb.jl")
+include("pariev-bromley-equations-klb.jl")
 
 # Define correlation length
-correlation_length = 1
+correlation_length = 10
 
-function turbulent_redshift(metric, x_obs, vel_func, correlation_length)
+turbulence_model = "fbm" # "perlin" or "fbm"
+
+function turbulent_redshift(metric, x_obs, vel_func, correlation_length, a, M, L, L_edd, r_ms, epsilon, mach)
     # Metric matrix at the observer's position
     g_obs = Gradus.metric(metric, x_obs)
     # Fixed stationary observer velocity
@@ -13,7 +19,7 @@ function turbulent_redshift(metric, x_obs, vel_func, correlation_length)
     # Internal closure function
     function _internal_turbulent_redshift(m::AbstractMetric, gp, t)
         # Pass correlation_length to vel_func
-        v_disc = vel_func(m, gp.x[2], gp.x[4], correlation_length)
+        v_disc = vel_func(m, gp.x[2], gp.x[4], correlation_length, a, M, L, L_edd, r_ms, epsilon, mach)
 
         g = Gradus.metric(m, gp.x)
         Gradus.RedshiftFunctions._redshift_dotproduct(g, v_disc, g_obs, v_obs, gp)
@@ -22,9 +28,11 @@ function turbulent_redshift(metric, x_obs, vel_func, correlation_length)
     return PointFunction(_internal_turbulent_redshift)
 end
 
-# Wrapper function that passes turb_func
-function velocity_wrapper(m, r, theta, correlation_length)
-    return turb_fbm(m, r, theta, correlation_length)
+# Wrapper function to select appropriate turbulence model
+function velocity_wrapper(m, r, theta, correlation_length, a, M, L, L_edd, r_ms, epsilon, mach)
+    if turbulence_model == "fbm"
+        return turb_fbm(m, r, theta, a, M, L, L_edd, r_ms, epsilon, correlation_length, mach)
+    end
 end
 
 # Parameters and initial setup
@@ -32,8 +40,18 @@ m = KerrMetric(1.0, 0.998)
 x = SVector(0.0, 1_000.0, deg2rad(80), 0.0)
 d = ThinDisc(Gradus.isco(m), 50.0)
 
+# Define parameters for turbulence model
+a = 0.998  # Black hole spin
+M = 1.0  # Black hole mass in geometrized units
+L = 1e46  # Luminosity in ergs/s
+L_edd = 1.2e46 * (M/1e8)  # Compute Eddington luminosity
+r_ms = Gradus.isco(m)  # Compute ISCO
+epsilon = 0.1  # Efficiency factor
+mach = 1  # Mach number
+
+
 # Compose our turbulent redshift function with a filter function to remove points outside of the ISCO
-redshift_pf = turbulent_redshift(m, x, velocity_wrapper, correlation_length)
+redshift_pf = turbulent_redshift(m, x, velocity_wrapper, correlation_length, a, M, L, L_edd, r_ms, epsilon, mach)
 pf = redshift_pf ∘ ConstPointFunctions.filter_intersected()
 
 # Render geodesics using the modified point function 
@@ -50,6 +68,7 @@ pf = redshift_pf ∘ ConstPointFunctions.filter_intersected()
     pf = pf,
 )
 
+# Plot ray trace geodesic image
 heatmap(α, β, img, aspect_ratio = 1)
 
 # Create and plot line profile 
@@ -59,7 +78,7 @@ _, f = lineprofile(m, x, d, redshift_pf = pf, verbose = true, method = BinningMe
 
 # Set whatever is in the last bin to 0 as it's most likely a noise contribution
 f[end] = 0
+
+# Plot line profile
 plot(bins, f, legend = false)
 
-# next step: to force correlation_length to increase with r, simulating much a much more chaotic, granular turbulence close 
-# to the black hole, and larger scale, less turbulent environment at larger r.
