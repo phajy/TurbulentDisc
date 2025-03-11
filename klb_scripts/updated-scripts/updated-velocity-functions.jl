73e960e8671e7f6a43265953b5df622b3e06d362
katebolam <katebolam.x@icloud.com>
2025-03-11 12:18:28 +0000

    feat: upload new script containing the turbulent velocity functions

---
+++ klb_scripts/updated-scripts/updated-velocity-functions.jl
@@ -0,0 +1,54 @@
+# This script contains the velocity functions for the turbulence models (Perlin/fBm)
+
+using Gradus, Plots, CoherentNoise
+include("updated-sound-speed.jl")
+
+function turbulence_perlin(m, r, theta, a, M, lum, correlation_length, mach; noise_r = perlin_2d(seed=1), noise_p = perlin_2d(seed=2), noise_a = perlin_2d(seed=3))
+
+    keplerian = Gradus.CircularOrbits.fourvelocity(m, r)
+
+    # Convert polar to Cartesian coordinates
+    x = r * cos(theta)
+    y = r * sin(theta)
+    
+    # Generate Perlin noise based on the Cartesian coordinates
+    noise_val_r = (1/sqrt(3))*mach*c(m, r, a, M, lum)*sample(noise_r, x/correlation_length, y/correlation_length)
+    noise_val_p = (1/sqrt(3))*mach*c(m, r, a, M, lum)*sample(noise_p, x/correlation_length, y/correlation_length)
+    noise_val_a = (1/sqrt(3))*mach*c(m, r, a, M, lum)*sample(noise_a, x/correlation_length, y/correlation_length)
+
+    vt = SVector(0, noise_val_r, noise_val_p, noise_val_a)
+    v = keplerian + vt
+
+    # Ensure velocity constraint
+    x = SVector(0.0, r, π/2, 0.0)
+    Gradus.constrain_all(m, x, v, 1.0)
+
+    return v
+            
+end
+
+function turbulence_fbm(m, r, theta, a, M, ratio, alpha, correlation_length, mach; noise_r = fbm_fractal_2d(seed=4), noise_p = fbm_fractal_2d(seed=5), noise_a = fbm_fractal_2d(seed=6))
+
+    keplerian = Gradus.CircularOrbits.fourvelocity(m, r)
+
+    # Convert polar to Cartesian coordinates
+    x = r * cos(theta)
+    y = r * sin(theta)
+    
+    # Generate Perlin noise based on the Cartesian coordinates
+    noise_val_r = (1/sqrt(3))*mach*SoundSpeed(m, r, a, M, ratio)*sample(noise_r, x/correlation_length, y/correlation_length)
+    noise_val_p = (1/sqrt(3))*mach*SoundSpeed(m, r, a, M, ratio)*sample(noise_p, x/correlation_length, y/correlation_length)
+    noise_val_a = (1/sqrt(3))*mach*SoundSpeed(m, r, a, M, ratio)*sample(noise_a, x/correlation_length, y/correlation_length)
+
+    radial_inflow = RadialSpeed(m, r, a, alpha, M, ratio)
+
+    v_additional = SVector(0, noise_val_r + radial_inflow, noise_val_p, noise_val_a)
+    v = keplerian + v_additional
+
+    # Ensure velocity constraint
+    x = SVector(0.0, r, π/2, 0.0)
+    Gradus.constrain_all(m, x, v, 1.0)
+
+    return v
+            
+end
