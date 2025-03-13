422ec9e37eb7c856c5e6776f02636e338c65c213
katebolam <katebolam.x@icloud.com>
2025-03-13 18:09:34 +0000

    chore: moved all turbulent line profiles into dir 'turbulent' and amended filepath in script accordingly

--- klb_scripts/updated-scripts/updated-turbulent-line-profiles.jl
+++ klb_scripts/updated-scripts/updated-turbulent-line-profiles.jl
@@ -24,7 +24,7 @@ turbulence_model = "fbm" # 'perlin' or 'fbm'
 
 opacity_values = Dict(1 => 1.0, 2 => 0.8, 5 => 0.6)
 
-output_dir = "C:\\Users\\Kate\\project\\TurbulentDisc\\updated_klb_plots\\line_profiles"
+output_dir = "C:\\Users\\Kate\\project\\TurbulentDisc\\updated_klb_plots\\line_profiles\\turbulent"
 mkpath(output_dir)
 
 # Function for turbulent redshift
